e76212c79a1f0fe658e6cd4f1b794f635d37b4f7
NicolasMontes0 <montes.nicolas18@gmail.com>
2023-04-02 22:44:17 +0200

    init project

---
+++ src/main/java/com/trellolike/TrelloLikeApplication.java
@@ -0,0 +1,13 @@
+package com.trellolike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloLikeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrelloLikeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/trellolike/TrelloLikeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trellolike;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloLikeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
